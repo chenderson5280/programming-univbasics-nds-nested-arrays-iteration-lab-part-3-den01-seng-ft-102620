6a3612258979a03e9fb80c456b9001f0c00bf75d
Christopher Henderson <chenderson5280@gmail.com>
2020-10-09 17:58:06 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -6,7 +6,7 @@ def join_nested_strings(src)
   while count < src.length do
     inner_count = 0
     while inner_count < src[count].length do
-      if src[count][inner_count].is_a? Strings
+      if src[count][inner_count].is_a? String
         result << src[count][inner_count]
       end
       inner_count+=1
